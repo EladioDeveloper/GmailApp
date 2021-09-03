17db38fc0f849bebd64a39458be25b9408427030
Eladio Junior Rodriguez Rodriguez <eladiojuniorrodriguez@gmail.com>
2021-09-03 14:32:01 -0400

    UI Started

---
+++ GmailApp/GmailApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using GmailApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GmailApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
